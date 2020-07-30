6f4852365cb77ea2a21d4db5806a8485b6fda567
olawale micheal juwon <olawalejuwon@gmail.com>
2020-07-30 01:34:42 +0100

    fix login

--- controllers/login_controllers.js
+++ controllers/login_controllers.js
@@ -1,3 +1,50 @@
-exports.LoginUser = (req, res) => {
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/users");
+const bCrypt = require('bcrypt');
+
+
+exports.LoginUser = async (req, res) => {
+  const { username, password} = req.body;
+
+  try {
+    if (!username && !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Username And Password Is Required"
+      });
+
+  }
+  const userFound = await UserModel.findOne({username: username});
+  if (userFound) {
+    const PasswordMatch = await bCrypt
+      .compare(userFound.password, password);
+    if (PasswordMatch) {
+      const apiToken = jwt.sign(
+        {
+          username: userFound.username,
+          password: userFound.password,
+          email: userFound.email
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "2d",
+        }
+      );
+      userFound.api_token = apiToken;
+      await userFound.save();
+      res.status(200).json({
+        success: true,
+        message: "Log in Succesfull",
+        data: {
+          statusCode: 200,
+          user: userFound,
+        },
+      });
+    }
+  }
+
+  } catch (error) {
+    
+  }
   res.status(200).json("testing login route");
 };
