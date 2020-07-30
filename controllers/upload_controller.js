f361dbdccfe0831f513ea95920d88ec95361fd43
Saratu Muazu <sarahembee71@gmail.com>
2020-07-30 17:16:44 +0100

    Comment out upload

--- controllers/upload_controller.js
+++ controllers/upload_controller.js
@@ -28,30 +28,30 @@ exports.create = async (req, res) => {
   }
 };
 
-exports.update = async (req, res) => {
-    const { title, text, type} = req.body;
-    console.log(req.userData);
-    try {
-      const uploadFound = await UploadModel.findOne({_id: req.params._id});
-      console.log(uploadFound);
-      let image_url = '';
-      let points = 0;
-      if(req.files) image_url = await fileUpload(req);
-      if(req.files) points += 1;
-      const uploads = new UploadModel({
-        title, text, type, image_url, points
-      });
-      console.log(points);
-      await uploads.update({_id: uploadFound._id});
-      return res.status(200).json({
-        succes: true,
-        messgae: "Post updated successfully"
-      });
-  } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
+// exports.update = async (req, res) => {
+//     const { title, text, type} = req.body;
+//     console.log(req.userData);
+//     try {
+//       const uploadFound = await UploadModel.findOne({_id: req.params._id});
+//       console.log(uploadFound);
+//       let image_url = '';
+//       let points = 0;
+//       if(req.files) image_url = await fileUpload(req);
+//       if(req.files) points += 1;
+//       const uploads = new UploadModel({
+//         title, text, type, image_url, points
+//       });
+//       console.log(points);
+//       await uploads.update({_id: uploadFound._id});
+//       return res.status(200).json({
+//         succes: true,
+//         messgae: "Post updated successfully"
+//       });
+//   } catch (error) {
+//     return res.status(400).json({
+//       success: false,
+//       message: error.message
+//     });
+//   }
+// };
   
--- routes/upload.js
+++ routes/upload.js
@@ -3,6 +3,6 @@ const upload = require("../controllers/upload_controller");
 const auth = require("../middleware/auth");
 
 router.post("/upload/create", auth, upload.create);
-router.post("/upload/:_id/update", auth, upload.update);
+// router.post("/upload/:_id/update", auth, upload.update);
 
 module.exports = router;
