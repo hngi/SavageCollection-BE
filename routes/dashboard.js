06baf4e56c6e76c2922c4e1198ebd53f78e21350
Lumen-X <onohsomtochukwubasil@gmail.com>
2020-08-01 08:06:12 +0100

    Added logout feature

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const dashboard = require("../controllers/dashboard_controller");
 const auth = require("../middleware/auth");
 
-router.get("/dashboard", auth, dashboard.getDashboard);
+router.get("/user/dashboard", auth, dashboard.getDashboard);
 
 module.exports = router;
